54c473e48fd9f89eeaaf514e85f4a90d3e20bd6e
Qobi1 <tolaganov03@list.ru>
2023-08-03 22:58:54 +0500

    'commit'

--- app1/management/commands/runbot.py
+++ app1/management/commands/runbot.py
@@ -6,7 +6,7 @@ TOKEN = "6630912635:AAGDR6ktgo1Bbbt77GUx-AbR76hWk_AflXA"
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        application = Application.builder().token("Token").read_timeout(7).get_updates_read_timeout(42).build()
+        application = Application.builder().token(TOKEN).build()
         application.add_handler(CommandHandler('start', start))
         application.add_handler(MessageHandler(filters.TEXT, handle_msg))
         application.add_handler(CallbackQueryHandler(inline_handler))
