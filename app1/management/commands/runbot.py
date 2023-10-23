6acadee7f4fc2007937d0ff840726db1fc1a2a10
Qobi1 <tolaganov03@list.ru>
2023-10-23 10:22:40 +0500

    'commit'

--- app1/management/commands/runbot.py
+++ app1/management/commands/runbot.py
@@ -1,13 +1,13 @@
 from django.core.management import BaseCommand
 from app1.views import *
 from telegram.ext import CommandHandler, MessageHandler, Application, filters, CallbackQueryHandler, ApplicationBuilder
-TOKEN = "6630912635:AAEybmbL8U68vJBP5meW-dP_BdFQhNbMhmc"
+TOKEN = "6166820369:AAGD7VukuFLK7YZ9qYg2YYmIc_25E7PMv_8"
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # application = Application.builder().token(TOKEN).build()
-        application = Application.builder().token(TOKEN).read_timeout(30).write_timeout(30).build()
+        application = Application.builder().token(TOKEN).build()
+        # application = Application.builder().token(TOKEN).read_timeout(30).write_timeout(30).build()
         application.add_handler(CommandHandler('start', start))
         application.add_handler(MessageHandler(filters.TEXT, handle_msg))
         application.add_handler(CallbackQueryHandler(inline_handler))
